d6717f847f5c9aa60bf20c87e0a79deff0fd035e
Zenaku19 <44754617+Zenaku19@users.noreply.github.com>
2018-11-07 18:00:57 -0600

    Add files via upload

---
+++ ingredientesBueno.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 08-11-2018 a las 00:59:05
+-- Versión del servidor: 10.1.36-MariaDB
+-- Versión de PHP: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyecto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ingredientes`
+--
+
+CREATE TABLE `ingredientes` (
+  `Id_ingrediente` int(11) NOT NULL,
+  `Nombre_Ingre` varchar(100) NOT NULL,
+  `Tipo_Ingre` varchar(100) NOT NULL,
+  `Costo_Ingre` int(11) NOT NULL,
+  `Unidad_de_Ingre` int(11) NOT NULL,
+  `Imagen_Ingre` longblob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `ingredientes`
+--
+ALTER TABLE `ingredientes`
+  ADD PRIMARY KEY (`Id_ingrediente`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `ingredientes`
+--
+ALTER TABLE `ingredientes`
+  MODIFY `Id_ingrediente` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
